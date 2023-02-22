507ea02163211945c77fc9afeae047dac2d7269e
ssh555 <89600078+ssh555@users.noreply.github.com>
2023-02-22 16:21:28 +0800

    Add Tools : ReflectTool
    1. ReflectTool添加只能C++调用的模板函数 : 获取|设置对象属性值
    2. ReflectTool添加可以C++、蓝图都能调用的函数:获取|设置对象属性值 [限定了类型只能是int,FString,float,FName,bool,int32]
    3. ReflectTool添加只能C++调用的模板函数:执行对象的函数 [0-5个参数均支持，但只有0个参数的可以蓝图调用

---
+++ Source/LightingSimulator/Private/Tools/ReflectToolFunctionLibrary.cpp
@@ -0,0 +1,309 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tools/ReflectToolFunctionLibrary.h"
+#include <UObject/NoExportTypes.h>
+#include <UObject/Class.h>
+#include <HAL/Platform.h>
+
+template<typename T>
+T UReflectToolFunctionLibrary::GetPropertyValue(UObject* Target, FString PropertyName)
+{
+	return GetPropertyValue<T>(Target, FName(*PropertyName));
+}
+
+template<typename T>
+T UReflectToolFunctionLibrary::GetPropertyValue(UObject* Target, FName PropertyName)
+{
+	FProperty* property = Target->GetClass()->FindPropertyByName(PropertyName);
+	T* value = property->ContainerPtrToValuePtr<T>(Target);
+
+	return *value;
+}
+
+int UReflectToolFunctionLibrary::GetIntPropertyValue(UObject* Target, FName PropertyName)
+{
+	return GetPropertyValue<int>(Target, PropertyName);
+}
+
+FString UReflectToolFunctionLibrary::GetFStringPropertyValue(UObject* Target, FName PropertyName)
+{
+	return GetPropertyValue<FString>(Target, PropertyName);
+
+}
+
+FName UReflectToolFunctionLibrary::GetFNamePropertyValue(UObject* Target, FName PropertyName)
+{
+	return GetPropertyValue<FName>(Target, PropertyName);
+}
+
+float UReflectToolFunctionLibrary::GetFloatPropertyValue(UObject* Target, FName PropertyName)
+{
+	return GetPropertyValue<float>(Target, PropertyName);
+}
+
+bool UReflectToolFunctionLibrary::GetBoolPropertyValue(UObject* Target, FName PropertyName)
+{
+	return GetPropertyValue<bool>(Target, PropertyName);
+}
+
+int32 UReflectToolFunctionLibrary::GetInt32PropertyValue(UObject* Target, FName PropertyName)
+{
+	return GetPropertyValue<int32>(Target, PropertyName);
+}
+
+
+
+template<typename T>
+bool UReflectToolFunctionLibrary::SetPropertyValue(UObject* Target, FString PropertyName, T value)
+{
+	return SetPropertyValue<T>(Target, FName(*PropertyName), value);
+}
+template<typename T>
+bool UReflectToolFunctionLibrary::SetPropertyValue(UObject* Target, FName PropertyName, T value)
+{
+	FProperty* property = Target->GetClass()->FindPropertyByName(PropertyName);
+	if (property != NULL) {
+		T* oldValue = property->ContainerPtrToValuePtr<T>(Target);
+		*oldValue = value;
+		return true;
+	}
+	return false;
+}
+
+bool UReflectToolFunctionLibrary::SetIntPropertyValue(UObject* Target, FName PropertyName, int value)
+{
+	return SetPropertyValue<int>(Target, PropertyName, value);
+}
+
+bool UReflectToolFunctionLibrary::SetFStringPropertyValue(UObject* Target, FName PropertyName, FString value)
+{
+	return SetPropertyValue<FString>(Target, PropertyName, value);
+}
+
+bool UReflectToolFunctionLibrary::SetFNamePropertyValue(UObject* Target, FName PropertyName, FName value)
+{
+	return SetPropertyValue<FName>(Target, PropertyName, value);
+}
+
+bool UReflectToolFunctionLibrary::SetFloatPropertyValue(UObject* Target, FName PropertyName, float value)
+{
+	return SetPropertyValue<float>(Target, PropertyName, value);
+
+}
+
+bool UReflectToolFunctionLibrary::SetBoolPropertyValue(UObject* Target, FName PropertyName, bool value)
+{
+	return SetPropertyValue<bool>(Target, PropertyName, value);
+
+}
+
+bool UReflectToolFunctionLibrary::SetInt32PropertyValue(UObject* Target, FName PropertyName, int32 value)
+{
+	return SetPropertyValue<int32>(Target, PropertyName, value);
+
+}
+
+bool UReflectToolFunctionLibrary::ExeFunction(UObject* Target, FName FunctionName)
+{
+	if (Target == nullptr) {
+		return false;
+	}
+	UFunction* Func = Target->FindFunction(FunctionName);
+	if (Func) {
+		// 1. 给方法所有参数分配空间,并初始化
+		uint8* AllFuncParam = static_cast<uint8*>(FMemory_Alloca(Func->ParmsSize));
+		FMemory::Memzero(AllFuncParam, Func->ParmsSize);
+
+		// 2. 给所有参数赋值
+
+		// 3. 调用方法
+		Target->ProcessEvent(Func, AllFuncParam);
+		return true;
+	}
+	return false;
+}
+
+template<typename T>
+bool UReflectToolFunctionLibrary::ExeFunction(UObject* Target, FName FunctionName, T& param)
+{
+	if (Target == nullptr) {
+		return false;
+	}
+	UFunction* Func = Target->FindFunction(FunctionName);
+	if (Func) {
+		// 1. 给方法所有参数分配空间,并初始化
+		uint8* AllFuncParam = static_cast<uint8*>(FMemory_Alloca(Func->ParmsSize));
+		FMemory::Memzero(AllFuncParam, Func->ParmsSize);
+
+		// 2. 给所有参数赋值
+		int i = 0;
+		for (TFieldIterator<FProperty> IteratorOfParam(Func); IteratorOfParam; ++IteratorOfParam, ++i)
+		{
+			FProperty* FuncParam = *IteratorOfParam;
+			FString FuncParamName = FuncParam->GetName();
+			if (i == 0) {
+				*FuncParam->ContainerPtrToValuePtr<T>(AllFuncParam) = param;
+			}
+		}
+
+		// 3. 调用方法
+		Target->ProcessEvent(Func, AllFuncParam);
+		return true;
+	}
+	return false;
+}
+
+template<typename T, typename U>
+bool UReflectToolFunctionLibrary::ExeFunction(UObject* Target, FName FunctionName, T& param1, U& param2)
+{
+	if (Target == nullptr) {
+		return false;
+	}
+	UFunction* Func = Target->FindFunction(FunctionName);
+	if (Func) {
+		// 1. 给方法所有参数分配空间,并初始化
+		uint8* AllFuncParam = static_cast<uint8*>(FMemory_Alloca(Func->ParmsSize));
+		FMemory::Memzero(AllFuncParam, Func->ParmsSize);
+
+		// 2. 给所有参数赋值
+		int i = 0;
+		for (TFieldIterator<FProperty> IteratorOfParam(Func); IteratorOfParam; ++IteratorOfParam, ++i)
+		{
+			FProperty* FuncParam = *IteratorOfParam;
+			FString FuncParamName = FuncParam->GetName();
+			if (i == 0) {
+				*FuncParam->ContainerPtrToValuePtr<T>(AllFuncParam) = param1;
+			}
+			else if (i == 1) {
+				*FuncParam->ContainerPtrToValuePtr<U>(AllFuncParam) = param2;
+			}
+		}
+
+		// 3. 调用方法
+		Target->ProcessEvent(Func, AllFuncParam);
+		return true;
+	}
+	return false;
+}
+
+template<typename T, typename U, typename P>
+bool UReflectToolFunctionLibrary::ExeFunction(UObject* Target, FName FunctionName, T& param1, U& param2, P& param3)
+{
+	if (Target == nullptr) {
+		return false;
+	}
+	UFunction* Func = Target->FindFunction(FunctionName);
+	if (Func) {
+		// 1. 给方法所有参数分配空间,并初始化
+		uint8* AllFuncParam = static_cast<uint8*>(FMemory_Alloca(Func->ParmsSize));
+		FMemory::Memzero(AllFuncParam, Func->ParmsSize);
+
+		// 2. 给所有参数赋值
+		int i = 0;
+		for (TFieldIterator<FProperty> IteratorOfParam(Func); IteratorOfParam; ++IteratorOfParam, ++i)
+		{
+			FProperty* FuncParam = *IteratorOfParam;
+			FString FuncParamName = FuncParam->GetName();
+			if (i == 0) {
+				*FuncParam->ContainerPtrToValuePtr<T>(AllFuncParam) = param1;
+			}
+			else if (i == 1) {
+				*FuncParam->ContainerPtrToValuePtr<U>(AllFuncParam) = param2;
+			}
+			else if(i == 2) {
+				*FuncParam->ContainerPtrToValuePtr<P>(AllFuncParam) = param3;
+
+			}
+		}
+
+		// 3. 调用方法
+		Target->ProcessEvent(Func, AllFuncParam);
+		return true;
+	}
+	return false;
+}
+
+template<typename T, typename U, typename P, typename Q>
+bool UReflectToolFunctionLibrary::ExeFunction(UObject* Target, FName FunctionName, T& param1, U& param2, P& param3, Q& param4)
+{
+	if (Target == nullptr) {
+		return false;
+	}
+	UFunction* Func = Target->FindFunction(FunctionName);
+	if (Func) {
+		// 1. 给方法所有参数分配空间,并初始化
+		uint8* AllFuncParam = static_cast<uint8*>(FMemory_Alloca(Func->ParmsSize));
+		FMemory::Memzero(AllFuncParam, Func->ParmsSize);
+
+		// 2. 给所有参数赋值
+		int i = 0;
+		for (TFieldIterator<FProperty> IteratorOfParam(Func); IteratorOfParam; ++IteratorOfParam, ++i)
+		{
+			FProperty* FuncParam = *IteratorOfParam;
+			FString FuncParamName = FuncParam->GetName();
+			if (i == 0) {
+				*FuncParam->ContainerPtrToValuePtr<T>(AllFuncParam) = param1;
+			}
+			else if (i == 1) {
+				*FuncParam->ContainerPtrToValuePtr<U>(AllFuncParam) = param2;
+			}
+			else if (i == 2) {
+				*FuncParam->ContainerPtrToValuePtr<P> (AllFuncParam) = param3;
+
+			}
+			else if (i == 3) {
+				*FuncParam->ContainerPtrToValuePtr<Q> (AllFuncParam) = param4;
+			}
+		}
+
+		// 3. 调用方法
+		Target->ProcessEvent(Func, AllFuncParam);
+		return true;
+	}
+	return false;
+}
+
+template<typename T, typename U, typename P, typename Q, typename W>
+bool UReflectToolFunctionLibrary::ExeFunction(UObject* Target, FName FunctionName, T& param1, U& param2, P& param3, Q& param4, W& param5)
+{
+	if (Target == nullptr) {
+		return false;
+	}
+	UFunction* Func = Target->FindFunction(FunctionName);
+	if (Func) {
+		// 1. 给方法所有参数分配空间,并初始化
+		uint8* AllFuncParam = static_cast<uint8*>(FMemory_Alloca(Func->ParmsSize));
+		FMemory::Memzero(AllFuncParam, Func->ParmsSize);
+
+		// 2. 给所有参数赋值
+		int i = 0;
+		for (TFieldIterator<FProperty> IteratorOfParam(Func); IteratorOfParam; ++IteratorOfParam, ++i)
+		{
+			FProperty* FuncParam = *IteratorOfParam;
+			FString FuncParamName = FuncParam->GetName();
+			if (i == 0) {
+				*FuncParam->ContainerPtrToValuePtr<T>(AllFuncParam) = param1;
+			}
+			else if (i == 1) {
+				*FuncParam->ContainerPtrToValuePtr<U>(AllFuncParam) = param2;
+			}
+			else if (i == 2) {
+				*FuncParam->ContainerPtrToValuePtr<P>(AllFuncParam) = param3;
+
+			}
+			else if (i == 3) {
+				*FuncParam->ContainerPtrToValuePtr<Q>(AllFuncParam) = param4;
+			}
+			else if (i == 4) {
+				*FuncParam->ContainerPtrToValuePtr<W>(AllFuncParam) = param5;
+			}
+		}
+
+		// 3. 调用方法
+		Target->ProcessEvent(Func, AllFuncParam);
+		return true;
+	}
+	return false;
+}
+
---
+++ Source/LightingSimulator/Public/Tools/ReflectToolFunctionLibrary.h
@@ -0,0 +1,123 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ReflectToolFunctionLibrary.generated.h"
+
+class UObject;
+
+/**
+ * 
+ */
+UCLASS()
+class LIGHTINGSIMULATOR_API UReflectToolFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+#pragma region 获取属性值
+	/// <summary>
+	/// 获取对象类的对应属性名的值
+	/// T 为该值的类型
+	/// </summary>
+	template<typename T>
+	UFUNCTION()
+	static T GetPropertyValue(UObject* Target, FString PropertyName);
+
+	/// <summary>
+	/// 获取对象类的对应属性名的值
+	/// T 为该值的类型
+	/// </summary>
+	template<typename T>
+	UFUNCTION()
+	static T GetPropertyValue(UObject* Target, FName PropertyName);
+
+#pragma region 方便蓝图测试调用
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|GetProperty")
+	static int GetIntPropertyValue(UObject* Target, FName PropertyName);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|GetProperty")
+	static FString GetFStringPropertyValue(UObject* Target, FName PropertyName);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|GetProperty")
+	static FName GetFNamePropertyValue(UObject* Target, FName PropertyName);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|GetProperty")
+	static float GetFloatPropertyValue(UObject* Target, FName PropertyName);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|GetProperty")
+	static bool GetBoolPropertyValue(UObject* Target, FName PropertyName);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|GetProperty")
+	static int32 GetInt32PropertyValue(UObject* Target, FName PropertyName);
+#pragma endregion
+
+#pragma endregion
+
+#pragma region 设置属性值
+	/// <summary>
+	/// 设置属性值
+	/// </summary>
+	template<typename T>
+	UFUNCTION()
+	static bool SetPropertyValue(UObject* Target, FString PropertyName, T value);
+
+	/// <summary>
+	/// 设置属性值
+	/// </summary>
+	template<typename T>
+	UFUNCTION()
+	static bool SetPropertyValue(UObject* Target, FName PropertyName, T value);
+
+#pragma region 方便蓝图测试调用
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|SetProperty")
+	static bool SetIntPropertyValue(UObject* Target, FName PropertyName, int value);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|SetProperty")
+	static bool SetFStringPropertyValue(UObject* Target, FName PropertyName, FString value);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|SetProperty")
+	static bool SetFNamePropertyValue(UObject* Target, FName PropertyName, FName value);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|SetProperty")
+	static bool SetFloatPropertyValue(UObject* Target, FName PropertyName, float value);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|SetProperty")
+	static bool SetBoolPropertyValue(UObject* Target, FName PropertyName, bool value);
+
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|SetProperty")
+	static bool SetInt32PropertyValue(UObject* Target, FName PropertyName, int32 value);
+#pragma endregion
+
+#pragma endregion
+
+
+#pragma region 执行函数
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|ExeFunction")
+	static bool ExeFunction(UObject* Target, FName FunctionName);
+
+	template<typename T>
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|ExeFunction")
+	static bool ExeFunction(UObject* Target, FName FunctionName, T& param);
+
+	template<typename T, typename U>
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|ExeFunction")
+	static bool ExeFunction(UObject* Target, FName FunctionName, T& param1, U& param2);
+
+	template<typename T, typename U, typename P>
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|ExeFunction")
+	static bool ExeFunction(UObject* Target, FName FunctionName, T& param1, U& param2, P& param3);
+
+	template<typename T, typename U, typename P, typename Q>
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|ExeFunction")
+	static bool ExeFunction(UObject* Target, FName FunctionName, T& param1, U& param2, P& param3, Q& param4);
+
+	template<typename T, typename U, typename P, typename Q, typename W>
+	UFUNCTION(BlueprintCallable, Category = "ReflectTool|ExeFunction")
+	static bool ExeFunction(UObject* Target, FName FunctionName, T& param1, U& param2, P& param3, Q& param4, W& param5);
+#pragma endregion
+
+
+
+};
